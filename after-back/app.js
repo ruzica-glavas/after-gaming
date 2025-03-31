d802b410bc4cdc03ebde2cb9ed8238a0956636be
VincenzoAmari <vincenzo.am99@gmail.com>
2025-03-31 12:33:08 +0200

    update app

--- after-back/app.js
+++ after-back/app.js
@@ -1,7 +1,16 @@
 import express from "express";
 const app = express();
-import cors from "cors"
+import cors from "cors";
 const port = process.env.SERVER_PORT || 3000;
+import {
+  security,
+  compress,
+  corsMiddleware,
+  logger,
+  validateDiscount,
+  validateCheckout,
+  errorHandler,
+} from "./middleware.js";
 
 //import router
 //import serverRouter from "./routers/serverRouter.js";
@@ -12,11 +21,15 @@ app.use(cors());
 //middleware express
 app.use(express.json());
 
-
-
-
-
+// Middleware
+app.use(security); // Sicurezza con Helmet
+app.use(compress); // Compressione delle risposte
+app.use(corsMiddleware); // CORS
+app.use(express.json()); // Parsing JSON
+app.use(logger); // Logger personalizzato
 
 app.listen(port, () => {
-    console.log(`Server avviato sulla porta: ${port}`);
+  console.log(`Server avviato sulla porta: ${port}`);
 });
+
+export default app;
