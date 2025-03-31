ea6046dfead1de8e08f09a40864c7ce19c700c8d
AlexLosciale <losciale17@gmail.com>
2025-03-31 11:52:00 +0200

    add:botton/img

--- after-front/data-test/data-test.js
+++ after-front/data-test/data-test.js
@@ -5,7 +5,7 @@ const gioco = {
     description: "Black Myth: Wukong è un GDR d'azione ispirato al romanzo classico cinese 'Il viaggio in Occidente'. Il giocatore controlla il Predestinato, una scimmia antropomorfa basata su Sun Wukong, affrontando nemici e sfide in un mondo affascinante.",
     price: 49.99,
     original_price: 59.99,
-    image_url: "https://upload.wikimedia.org/wikipedia/en/6/6e/Black_Myth_Wukong_cover_art.jpg",
+    image_url: "https://shared.fastly.steamstatic.com/store_item_assets/steam/apps/2358720/f40ef565c380c617020e559b4b4b089edd93ec09/capsule_616x353.jpg?t=1739542141",
     platform: ["PlayStation 5", "Windows", "Xbox Series X/S"],
     trailer_url: "https://www.youtube.com/watch?v=O2nNljv0MOw",
     created_at: new Date("2024-08-20")
--- after-front/src/components/MainDettaglio.jsx
+++ after-front/src/components/MainDettaglio.jsx
@@ -1,18 +1,37 @@
 import gioco from "../../data-test/data-test";
+import { useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faFire, faCartShopping, faHeart } from "@fortawesome/free-solid-svg-icons";
 
 export default function MainDettaglio() {
+    const [usersOnline, setUsersOnline] = useState(0);
+
+    useEffect(() => {
+        setUsersOnline(Math.floor(Math.random() * (1000 - 100 + 1)) + 100);
+    }, []);
+
     return (
-        <div className="container">
+        <div className="container mt-5">
             <div className="row text-center">
                 <div className="col-6">
                     <img src={gioco.image_url} alt={gioco.name} />
                 </div>
                 <div className="col-6">
                     <h1>{gioco.name}</h1>
-                    <p>{gioco.description}</p>
                     <p>Prezzo: {gioco.price} €</p>
                     <p>Prezzo originale: {gioco.original_price} €</p>
                     <p>Disponibile per: {gioco.platform.join(", ")}</p>
+                    <p> users on this page: {usersOnline}
+                        <FontAwesomeIcon icon={faFire} color="red" style={{ marginLeft: "3px" }} />
+                    </p>
+                    <div className="d-flex justify-content-center gap-3 mt-5">
+                        <button>
+                            <FontAwesomeIcon icon={faHeart} style={{ color: "grey", fontSize: "24px" }} />Add to wishlist
+                        </button>
+                        <button>
+                            <FontAwesomeIcon icon={faCartShopping} style={{ color: "grey", fontSize: "24px" }} />Add to cart
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
