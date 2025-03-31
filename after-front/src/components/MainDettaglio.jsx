91c2b9fa1ccf7e696228aef8f54d6700ce1fce04
AlexLosciale <losciale17@gmail.com>
2025-03-31 11:21:43 +0200

    add: dettaglio

--- after-front/src/components/MainDettaglio.jsx
+++ after-front/src/components/MainDettaglio.jsx
@@ -1,7 +1,20 @@
+import gioco from "../../data-test/data-test";
+
 export default function MainDettaglio() {
     return (
-        <div>
-            <h1>MainDettaglio</h1>
+        <div className="container">
+            <div className="row text-center">
+                <div className="col-6">
+                    <img src={gioco.image_url} alt={gioco.name} />
+                </div>
+                <div className="col-6">
+                    <h1>{gioco.name}</h1>
+                    <p>{gioco.description}</p>
+                    <p>Prezzo: {gioco.price} €</p>
+                    <p>Prezzo originale: {gioco.original_price} €</p>
+                    <p>Disponibile per: {gioco.platform.join(", ")}</p>
+                </div>
+            </div>
         </div>
-    )
-};
+    );
+}
