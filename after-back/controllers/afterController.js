d637d37faf77fb48c119517a74700da22fb4fe16
VincenzoAmari <vincenzo.am99@gmail.com>
2025-04-01 11:12:54 +0200

    add new routes

--- after-back/controllers/afterController.js
+++ after-back/controllers/afterController.js
@@ -1,14 +1,61 @@
 import db from "../data/db.js";
 
 export function getProducts(req, res) {
-  const sql = "SELECT * FROM products";
+  let sql = "SELECT * FROM products";
+  const params = [];
+  const { sort, filter } = req.query;
 
-  db.query(sql, (err, results) => {
+  if (filter) {
+    const filters = filter.split(",").map((f) => f.split("="));
+    filters.forEach(([key, value]) => {
+      sql += ` WHERE ${key} = ?`;
+      params.push(value);
+    });
+  }
+  if (sort) {
+    const [field, order] = sort.split("-");
+    sql += ` ORDER BY ${field} ${order === "desc" ? "DESC" : "ASC"}`;
+  }
+
+  db.query(sql, params, (err, results) => {
     if (err) {
       return res
         .status(500)
         .json({ error: "Errore lato server in getProducts" });
     }
     res.json(results);
+  });
+}
+
+export function getLatestProducts(req, res) {
+  const sql = "SELECT * FROM products ORDER BY created_at DESC LIMIT 5";
+
+  db.query(sql, (err, results) => {
+    if (err) {
+      return res
+        .status(500)
+        .json({ error: "Errore lato server in getLatestProducts" });
+    }
+    res.json(results);
+  });
+}
+
+export function getProductBySlug(req, res) {
+  if (!req.db)
+    return res
+      .status(500)
+      .json({ error: "Connessione al database non disponibile" });
+  const { slug } = req.params; // Usiamo slug invece di id
+  const productsSql = "SELECT * FROM products WHERE slug = ?";
+
+  req.db.query(productsSql, [slug], (err, results) => {
+    if (err)
+      return res
+        .status(500)
+        .json({ error: "Errore lato server in getProductBySlug" });
+
+    if (results.length === 0)
+      return res.status(404).json({ error: "Prodotto non trovato" });
+    res.json(results[0]);
   });
 }
--- after-back/routers/serverRouter.js
+++ after-back/routers/serverRouter.js
@@ -1,9 +1,15 @@
 import express from "express";
-import { getProducts } from "../controllers/afterController.js";
+import {
+  getProducts,
+  getLatestProducts,
+  getProductBySlug,
+} from "../controllers/afterController.js";
 
 const router = express.Router();
 
 // Definizione delle API
 router.get("/products", getProducts);
+router.get("/latest-products", getLatestProducts);
+router.get("/products/:slug", getProductBySlug);
 
 export default router;
