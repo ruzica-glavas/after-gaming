038ca49f5e5eafdafe57d828a2b308f99911c9ea
AlexLosciale <losciale17@gmail.com>
2025-04-01 21:35:52 +0200

    add: text css

--- after-front/src/components/MainHomepage.jsx
+++ after-front/src/components/MainHomepage.jsx
@@ -8,17 +8,17 @@ export default function MainHomepage() {
     return (
         <>
             <div className="container d-flex flex-column align-items-center justify-content-center">
-                <h3 className='text-white'>Imperdibili</h3>
+                <h3 className='text-white text-uppercase fw-bold'> Imperdibili </h3>
                 <Carosello />
             </div>
             <div>
                 <div className="container d-flex flex-column pt-5 pb-4">
-                    <h4 className="px-4 pb-3 text-white">Ultime uscite {'\u2192'}</h4>
+                    <h4 className="px-4 pb-3 text-white">Le ultime novità {'\u2794'}</h4>
                     <UltimiArrivi />
                 </div>
 
                 <div className="container d-flex flex-column pt-5 pb-4">
-                    <h4 className="px-4 pb-3 text-white">Tendenze {'\u2192'}</h4>
+                    <h4 className="px-4 pb-3 text-white">Le tendenze del momento {'\u2794'}</h4>
                     <Tendenze />
                 </div>
             </div>
