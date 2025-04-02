00dfb15305738c35fb9c471f9fb92465d2974e7b
AlexLosciale <losciale17@gmail.com>
2025-04-02 10:49:59 +0200

    add: fix color

--- after-front/src/components/Ricerca.jsx
+++ after-front/src/components/Ricerca.jsx
@@ -48,8 +48,8 @@ export default function Ricerca() {
                                 />
                             </Link>
                             <div className="d-flex justify-content-between align-items-center w-100 mt-2">
-                                <p className="game-name">{game.name}</p>
-                                <p className="game-price">{game.price}€</p>
+                                <p className="game-name text-white">{game.name}</p>
+                                <p className="game-price text-white">{game.price}€</p>
                             </div>
                         </div>
                     ))
--- after-front/src/components/UltimiArrivi.jsx
+++ after-front/src/components/UltimiArrivi.jsx
@@ -66,7 +66,7 @@ export default function UltimiArrivi() {
                                 />
                             </Link>
                             <div className="d-flex justify-content-between align-items-center w-100 mt-2">
-                                <p className="game-name">{game.name}</p>
+                                <p className="game-name text-white">{game.name}</p>
                                 <p className="game-price">
                                     {game.original_price && (
                                         <span className="original-price">{game.original_price}€</span>
