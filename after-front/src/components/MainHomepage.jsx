700c3865a37e699c4b5aa9af56b5ec420e0f1f15
AlexLosciale <losciale17@gmail.com>
2025-04-07 15:46:01 +0200

    add: prop benvenuto

--- after-front/src/components/MainHomepage.jsx
+++ after-front/src/components/MainHomepage.jsx
@@ -3,15 +3,18 @@ import Carosello from './Carosello'
 import UltimiArrivi from './UltimiArrivi.jsx'
 import Tendenze from './Tendenze.jsx'
 import { Link } from 'react-router-dom'
+import WelcomePopup from './WelcomePopup.jsx'
 
 export default function MainHomepage() {
-
     return (
         <>
+            <WelcomePopup /> {/* <-- blocca la pagina finché non chiuso */}
+            
             <div className="container d-flex flex-column align-items-center justify-content-center">
                 <h3 className='carousel-title text-white py-4 fw-bold'>------- IMPERDIBILI -------</h3>
                 <Carosello />
             </div>
+
             <div>
                 <div className="container d-flex flex-column pt-5 pb-4">
                     <h4 className="text-white px-4 pb-3">Tendenze <Link to={`/tendenze`}><button className="link-button">{'\u2794'}</button></Link></h4>
