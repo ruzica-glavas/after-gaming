82d4c5e5a4e4e24444bcb7601aed770371d01900
Giovanni-Greggio7 <giovannigreggio2@gmail.com>
2025-04-09 17:14:59 +0200

    fix: correct import paths for Header and Footer components

--- after-front/src/pages/NotFound.jsx
+++ after-front/src/pages/NotFound.jsx
@@ -1,6 +1,6 @@
 import { Link } from "react-router-dom";
-import Header from "../components/Header";
-import Footer from "../components/Footer";
+import Header from "../components/header";
+import Footer from "../components/footer";
 import DoomGuy404 from "../public/imgs/ChatGPT Image 9 apr 2025, 11_15_46.png";
 
 export default function NotFound() {
